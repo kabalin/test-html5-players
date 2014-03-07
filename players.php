21ba7a8b164511288a8d813c00b6bf09f765a682
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2014-03-07 11:22:07 +0000

    Add GPL header.

--- players.php
+++ players.php
@@ -1,4 +1,23 @@
 <?php
+// This file is part of HTML5 Players Test.
+// Copyright (C) 2014 Ruslan Kabalin, Lancaster University <r.kabalin@lancaster.ac.uk>
+//
+// HTML5 Players Test is free software: you can redistribute
+// it and/or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// HTML5 Players Test is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with HTML5 Players Test.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+// For details see: http://docs.moodle.org/dev/HTML5_player
+//
 $player = $_GET['player'];
 
 $players = array('flowplayer', 'videojs', 'projekktor');
